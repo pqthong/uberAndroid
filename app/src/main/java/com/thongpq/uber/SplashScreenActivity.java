6c8966286877717b72b3c14811443151a8e6b30b
thongpq <thongpq@d-soft.com.vn>
2022-11-06 09:13:31 +0700

    splash screen

--- app/src/main/java/com/thongpq/uber/SplashScreenActivity.java
+++ app/src/main/java/com/thongpq/uber/SplashScreenActivity.java
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
