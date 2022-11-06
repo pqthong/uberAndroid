0c3256df8d33e76d2166a1a7b50206e09e3853fa
thongpq <thongpq@d-soft.com.vn>
2022-11-06 12:49:04 +0700

    online system

--- app/src/main/java/com/thongpq/uber/Common.java
+++ app/src/main/java/com/thongpq/uber/Common.java
@@ -5,6 +5,8 @@ import com.thongpq.uber.Model.DriverInfoModel;
 public class Common {
 
     public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+    public static final String DRIVERS_LOCATION_REFERENCES = "DriversLocation";
+
     public static DriverInfoModel currentUser;
 
 }
--- app/src/main/java/com/thongpq/uber/ui/home/HomeFragment.java
+++ app/src/main/java/com/thongpq/uber/ui/home/HomeFragment.java
@@ -15,6 +15,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -30,12 +32,20 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
+import com.thongpq.uber.Common;
 import com.thongpq.uber.R;
 import com.thongpq.uber.SplashScreenActivity;
 import com.thongpq.uber.databinding.FragmentHomeBinding;
@@ -51,10 +61,38 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private SupportMapFragment mapFragment;
 
+    DatabaseReference onlineRef, currentUserRef, driversLocationRef;
+    GeoFire geoFire;
+    ValueEventListener onlineValueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            if(snapshot.exists()){
+                currentUserRef.onDisconnect().removeValue();
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Snackbar.make(mapFragment.getView(), error.getMessage(), Snackbar.LENGTH_LONG).show();
+        }
+    };
+
     @Override
     public void onDestroy() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        geoFire.removeLocation(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        onlineRef.removeEventListener(onlineValueEventListener);
         super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerOnlineSystem();
+    }
+
+    private void registerOnlineSystem(){
+        onlineRef.addValueEventListener(onlineValueEventListener);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -72,6 +110,12 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     @SuppressLint("MissingPermission")
     private void init() {
+        onlineRef = FirebaseDatabase.getInstance().getReference().child(".info/connected");
+        driversLocationRef = FirebaseDatabase.getInstance().getReference(Common.DRIVERS_LOCATION_REFERENCES);
+        currentUserRef = FirebaseDatabase.getInstance().getReference(Common.DRIVERS_LOCATION_REFERENCES)
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        geoFire = new GeoFire(driversLocationRef);
+
         locationRequest = new LocationRequest.Builder(5000)
                 .setMinUpdateDistanceMeters(10f)
                 .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
@@ -84,6 +128,18 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
                 LatLng newPosition = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition, 18f));
 
+                geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(),
+                        new GeoLocation(locationResult.getLastLocation().getLatitude(),
+                                locationResult.getLastLocation().getLongitude()),
+                        (key, error) -> {
+                            if (error != null){
+                                Snackbar.make(mapFragment.getView(), error.getMessage(), Snackbar.LENGTH_LONG).show();
+                            } else {
+                                Snackbar.make(mapFragment.getView(), "You are online", Snackbar.LENGTH_LONG).show();
+
+                            }
+                        }
+                );
             }
         };
 
